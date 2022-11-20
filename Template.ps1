ad4052a3edf3ed8a6f4c3a212bb2813bbca7295e
Vertamin <manuel-angel2@hotmail.es>
2022-11-20 12:08:43 +0100

    New scripts for templates

--- Template.ps1
+++ Template.ps1
@@ -1,1 +1,3 @@
 Connect-PnPOnline -Url "https://8vbkqy.sharepoint.com/sites/ignitenursing" -Interactive
+
+Get-pnpsitetemplate -Out "Lists.xml" -Handlers Lists -ListsToExtract @("MegaMenu","MyLinks","MegaMenuParameters")
--- TemplateApply.ps1
+++ TemplateApply.ps1
@@ -1,1 +1,3 @@
 Connect-PnPOnline -Url "https://handfcarecom.sharepoint.com/sites/ignitecustomsapp" -Interactive
+
+Invoke-PnPSiteTemplate -Path "Lists.xml"
